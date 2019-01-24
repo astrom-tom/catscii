cac2665048f34721348c7d733ea22a0df1d69ab0
romain <the.spartan.proj@gmail.com>
2019-01-24 16:17:45 -0300

    packaging

---
+++ catscii/__info__.py
@@ -0,0 +1,9 @@
+__version__ = 0.1
+__author__ = 'R. THOMAS'
+__licence__ = 'GPLv3'
+__credits__ = "Romain Thomas"
+__maintainer__ = "Romain Thomas"
+__email__ = "the.spartan.proj@gmail.com"
+__status__ = "dev"
+__year__ = '2018-19'
+
